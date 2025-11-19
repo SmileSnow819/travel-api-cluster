4616c94f431a82adc880e97e6f2f2c41c6a8a2a3
SmileSnow819 <2465225759@qq.com>
2025-11-19 21:32:35 +0800

    feat：增加404页面 修复重定向错误

--- natours-frontend/src/router/index.ts
+++ natours-frontend/src/router/index.ts
@@ -4,6 +4,10 @@ import routes from './routes';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    // 在GitHub Pages上总是滚动到顶部
+    return { top: 0 };
+  },
 });
 
 export default router;
